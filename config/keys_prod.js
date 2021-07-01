9ec447fb177a91c9c2c6f9330f292cf1dd3db9db
enzo <florenzodinglasan@gmail.com>
2021-07-01 13:34:41 +0800

    Added post feature

---
+++ config/keys_prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+    sID: process.env.TWILIO_ACCOUNT_SID,
+    aToken: process.env.TWILIO_AUTH_TOKEN,
+    host:process.env.HOST,
+    user: process.env.USER,
+    pass: process.env.PASSWORD,
+    database:process.env.DATABASE
+};
---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const app = express()
+const mysql = require('mysql')
+const cors = require('cors')
+
+const port = process.env.PORT || 4002;
+
+const db = mysql.createPool({
+    connectionLimit : 10,
+    host    :require('./config/keys').host,
+    user    :require('./config/keys').user,
+    password:require('./config/keys').pass,
+    database:require('./config/keys').database
+});
+
+app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({extended:true}));
+
+// db.connect((err)=>{if(err){throw err} console.log('db is connected');});
+
+app.post('/api/reservation',(req,res)=>{
+
+    const customer = req.body.name;
+    const schedule = req.body.body;
+    // const pax = req.body.pax;
+    // const contact = req.body.contact
+    // const resDate = Date.now
+    // const cusRequest = req.body.customRequest
+
+    //***mysql query */
+    // const sqlInsert = "INSERT INTO message (customer, schedule, pax, contact, resDate) VALUES (?,?,?,?)";
+    // db.query(sqlInsert, [customer, schedule, pax,resDate], (err,result)=>{
+    //     console.log(result);
+    // })
+
+
+    const sqlInsert = "INSERT INTO message (name, body) VALUES (?,?)";
+    db.query(sqlInsert, [customer, schedule], (err,result)=>{
+    });
+
+
+// const accountSid = require('./config/keys').sID;
+// const authToken = require('./config/keys').aToken;
+// const fromNumber = require('./config/keys').from;
+// const reciever =require('./config/keys').reciever
+// const client = require('twilio')(accountSid, authToken);
+
+// client.messages
+//   .create({
+//      body: 'customer: ${customer} \nSchedule: ${schedule} \nPax: ${pax} \nContact: ${contact} \nRequest: ${request}',
+//      from: `+${fromNumber}`,
+//      to: `+${reciever}` 
+//    })
+//   .then(message => console.log(message.sid));
+
+});
+
+
+app.listen(port,()=>{
+    console.log(`server at port ${port}`);
+});
+
